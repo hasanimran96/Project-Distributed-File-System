170ddd476e6115b3c08f9b821ceb0847dc8c3e43
Hasan Imran <hasanimran_96@yahoo.com>
2018-11-11 19:33:30 +0500

    client/server connection using socket

--- client.py
+++ client.py
@@ -1,95 +1,123 @@
 import os
 import socket
 
-#set directory for file server
+# set directory for file server
 root = "Root/"
 
+
 def open_file(file_name):
-    fd = open(root+file_name)
+    fd = open(root + file_name)
     return fd
+
 
 def close_file(fd):
     fd.close()
 
+
 def create_file(file_name):
-    fd = open(root+file_name,'x')
+    fd = open(root + file_name, "x")
     return fd
 
+
 def read_from_file(file_name):
-    fd = open(root+file_name,'r')
+    fd = open(root + file_name, "r")
     message = fd.read()
     return message
 
-def write_to_file(file_name,message):
-    fd = open(root+file_name,'w')
+
+def write_to_file(file_name, message):
+    fd = open(root + file_name, "w")
     fd.write(message)
 
-def append_to_file(file_name,message):
-    fd = open(root+file_name,'a')
+
+def append_to_file(file_name, message):
+    fd = open(root + file_name, "a")
     fd.write(message)
+
 
 def list_files(directory):
     temp_list = os.listdir(directory)
     for file in temp_list:
         print(file)
 
+
 def create_directory(directory_name):
     if not os.path.exists(root + directory_name):
         os.mkdir(root + directory_name)
-        print("Directory " , directory_name ,  " Created ")
-    else:    
-        print("Directory " , directory_name ,  " already exists")
+        print("Directory ", directory_name, " Created ")
+    else:
+        print("Directory ", directory_name, " already exists")
+
+
+def make_socket():
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # get local machine name
+    host = socket.gethostname()
+
+    port = 9999
+
+    # connection to hostname on the port.
+    s.connect((host, port))
+
+    return s
+
 
 def main():
     print("Project DFS!")
     print("Write a command to execute or type help")
 
-    # create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
-    # get local machine name
-    host = socket.gethostname()                           
-
-    port = 9999
-
-    # connection to hostname on the port.
-    s.connect((host, port))                               
-
     # Receive no more than 1024 bytes
-    msg = s.recv(1024)                                     
+    s = make_socket()
 
-    s.close()
-    print (msg.decode('ascii'))
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
 
-    while True:    
-        command = input()
-        command_split = command.split()
-        if(len(command_split)<1):
-            print("Please write a command or type help")
-        elif(command_split[0]=="open"):
-            open_file(command_split[1])
-        elif(command_split[0]=="read"):
-            message = read_from_file(command_split[1])
-            print(message)
-        elif(command_split[0]=="create"):
-            create_file(command_split[1])
-        elif(command_split[0]=="write"):
-            str_temp = " ".join(str(x) for x in command_split[2:])
-            write_to_file(command_split[1],str_temp)
-        elif(command_split[0]=="append"):
-            str_temp = " ".join(str(x) for x in command_split[2:])
-            append_to_file(command_split[1],str_temp)
-        elif(command_split[0]=="list"):
-            if(len(command_split)>2):
-                list_files(command_split[1])
-            else:
-                list_files("Root")
-        elif(command_split[0]=="mkdir"):
-            create_directory(command_split[1])
-        #elif(command_split[0]=="close"):
-        elif(command_split[0]=="exit"):
-            break
+    while True:
+
+        print("Server says enter your command")
+        prompt = input()
+
+        if prompt == "close":
+            s.send(prompt.encode("utf-8"))
+            s.close()
         else:
-            print("Invalid intruction. Type help")
+            s.send(prompt.encode("utf-8"))
+
+        msg = s.recv(1024)
+        print(msg.decode("utf-8"))
+
+    # while True:
+    #     command = input()
+    #     command_split = command.split()
+    #     if len(command_split) < 1:
+    #         print("Please write a command or type help")
+    #     elif command_split[0] == "open":
+    #         open_file(command_split[1])
+    #     elif command_split[0] == "read":
+    #         message = read_from_file(command_split[1])
+    #         print(message)
+    #     elif command_split[0] == "create":
+    #         create_file(command_split[1])
+    #     elif command_split[0] == "write":
+    #         str_temp = " ".join(str(x) for x in command_split[2:])
+    #         write_to_file(command_split[1], str_temp)
+    #     elif command_split[0] == "append":
+    #         str_temp = " ".join(str(x) for x in command_split[2:])
+    #         append_to_file(command_split[1], str_temp)
+    #     elif command_split[0] == "list":
+    #         if len(command_split) > 2:
+    #             list_files(command_split[1])
+    #         else:
+    #             list_files("Root")
+    #     elif command_split[0] == "mkdir":
+    #         create_directory(command_split[1])
+    #     # elif(command_split[0]=="close"):
+    #     elif command_split[0] == "exit":
+    #         break
+    #     else:
+    #         print("Invalid intruction. Type help")
+
 
 main()
--- server.py
+++ server.py
@@ -1,26 +1,49 @@
 import socket
 
-# create a socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# get local machine name
-host = socket.gethostname()
+def create_socket():
+    # create a socket object
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-port = 9999
+    # get local machine name
+    host = socket.gethostname()
 
-# bind to the port
-serversocket.bind((host, port))
+    port = 9999
 
-# queue up to 5 requests
-serversocket.listen(5)
+    # bind to the port
+    serversocket.bind((host, port))
 
-while True:
+    return serversocket
+
+
+def main():
+
     # establish a connection
-    clientsocket, addr = serversocket.accept()
+    serversocket = create_socket()
 
-    print("Got a connection from %s" % str(addr))
+    # queue up to 5 requests
+    serversocket.listen(5)
 
-    msg = "Thank you for connecting" + "\r\n"
-    clientsocket.send(msg.encode("ascii"))
-    clientsocket.close()
+    while True:
 
+        clientsocket, addr = serversocket.accept()
+
+        print("Got a connection from %s" % str(addr))
+
+        msg = "Thank you for connecting"
+        clientsocket.send(msg.encode("utf-8"))
+
+        while True:
+
+            msg = clientsocket.recv(1024)
+            if msg.decode("utf-8") == "close":
+                clientsocket.close()
+            else:
+                if msg.decode("utf-8") == "download":
+                    msg = "Here is your downloaded file haha"
+                    clientsocket.send(msg.encode("utf-8"))
+                else:
+                    clientsocket.send(msg)
+
+
+main()
